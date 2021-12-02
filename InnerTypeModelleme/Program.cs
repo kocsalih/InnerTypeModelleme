99861d9a606d653003e93ad983e7bdda49649a54
salihkoc <salihkoc-e@users.noreply.github.com>
2021-12-02 16:21:46 +0300

    v2

--- InnerTypeModelleme/Program.cs
+++ InnerTypeModelleme/Program.cs
@@ -10,6 +10,44 @@ namespace InnerTypeModelleme
     {
         static void Main(string[] args)
         {
+            Musteri M1 = new Musteri();
+            M1.musteriId = 1;
+            M1.tcKimlikNumarasi = "15879625120";
+            M1.isim = "faik";
+            M1.soyIsim = "turan";
+            M1.olusturmaTarih = DateTime.Now;
+
+            M1.musteriAdresleri[0] = new MusteriAdres()
+            {
+                il = "istanbul",
+                ilce = "kadıköy",
+                adres = "boğanın yanı",
+                adreTip = "iş yeri"
+            };
+            M1.musteriAdresleri[0].MusteriAdresTestMetot();
+
+            M1.musteriSiparisBilgileri[0] = new MusteriSiparisBilgisi()
+            {
+                siparisNumarasi = "Sp00001"
+            };
+
+
+            M1.musteriSiparisBilgileri[0].urunler[0] = new Urun()
+            {
+                UrunId = 1,
+                tanim = "telefon",
+                fiyat = 9000
+            };
+
+            M1.musteriSiparisBilgileri[0].urunler[1] = new Urun()
+            {
+                UrunId = 2,
+                tanim = "bilgisayar",
+                fiyat = 13000
+            };
+
+         
+            Console.ReadLine();
         }
     }
 }
